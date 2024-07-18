565e70eb30b95677d479fd682aadd2c073fc4333
Porter Christensen <porter.christensen@circle.com>
2024-07-18 23:44:14 +0000

    redirect to shop when at index

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      test
-    </main>
-  );
+import { redirect } from "next/navigation";
+
+export default function Page() {
+  redirect("/shop");
 }
